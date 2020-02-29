294292eec49c6d5fafd1f2c194668a3acdc37a6a
Jenny Guzdial <jguzdial6@gatech.edu>
2020-02-29 17:30:50 -0500

    Added pretty stuff to WinScreen

--- screens/WinScreen.js
+++ screens/WinScreen.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 export default function WinScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.wordMa}>
-        WinScreen
+        <Text style={styles.winnerText}>Player 1 </Text>You Win!!
       </Text>
+      <View>
+      <Image
+          source={{
+            uri:
+            'https://i.imgur.com/baOJsZ4.png'
+          }}
+        style={{width: 150, height: 100, alignSelf: 'center'}} 
+        />
+    </View>
     </View >
   );
 }
@@ -16,7 +25,13 @@ WinScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+
   wordMa: {
+    fontFamily: 'lobster-regular',
     fontSize: 20,
+    alignSelf: 'center'
+  },
+  winnerText: {
+    color: 'blue',
   }
 });

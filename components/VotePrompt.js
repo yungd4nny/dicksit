f37752ad52f5849ee24729f8318e3b72c8a7cdcc
drxwbailxy <dbailey28782@gmail.com>
2020-02-29 19:41:53 -0500

    style change for VotePrompt

--- components/VotePrompt.js
+++ components/VotePrompt.js
@@ -1,34 +1,47 @@
 import * as React from "react";
-import {Text, View,onChangeText, StyleSheet} from 'react-native';
+import { Text, View, onChangeText, StyleSheet } from 'react-native';
 
-export default function VotePrompt({playerName,phrase, wordPlayer}){
-    return<View style={styles.VotePromptContainer}>
-         <Text style={styles.votePromptPlayerText}> {playerName}</Text>
-            <Text>
+export default function VotePrompt({ playerName, phrase, wordPlayer }) {
+    return <View style={styles.VotePromptContainer}>
+        <Text style={styles.votePromptPlayerText}> {playerName}</Text>
+        <Text style={{ fontFamily: 'federant-regular', fontSize: 16 }}>
             pick the card <Text style={styles.votePromptWordPlayerText}>{wordPlayer}</Text> chose that best fits the phrase:
             </Text>
-         <Text style={styles.votePromptPhraseText}>
-             {phrase}
-         </Text>
-    </View>
+        <Text style={styles.votePromptPhraseText}>
+            {phrase}
+        </Text>
+    </View >
 }
 const styles = StyleSheet.create({
     VotePromptContainer: {
-      alignSelf: 'center',
-      borderWidth: 1,
-      borderColor: 'black',
-      alignItems: 'center',
-      justifyContent: 'center',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: '2%',
+        width: '90%',
+        height: '50%',
+        fontFamily: 'federant-regular'
 
     },
     votePromptPlayerText: {
-        color: 'blue',
-        fontSize: 20
+        color: 'lightgreen',
+        fontSize: 45,
+        paddingRight: 10,
+        textAlign: 'center',
+        fontFamily: 'lobster-regular'
+
     },
     votePromptWordPlayerText: {
-        color: 'red'
+        color: 'red',
+        fontFamily: 'federant-regular'
     },
     votePromptPhraseText: {
-        fontSize: 20
+        color: 'gold',
+        fontSize: 30,
+        fontFamily: 'federant-regular',
+        fontWeight: 'bold'
+
     }
 })

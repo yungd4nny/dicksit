7e479cf93f79a8d6256f3f1c3d8defb94a9c865f
David Gamero <david340804@gmail.com>
2020-03-02 12:36:25 -0500

    choose phase card picking

--- screens/ChoosePhaseScreen.js
+++ screens/ChoosePhaseScreen.js
@@ -1,15 +1,29 @@
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import WordPhasePrompt from '../components/WordPhasePrompt';
+import CardPicker from '../components/CardPicker';
 
 export default function ChoosePhaseScreen({ navigation, route }) {
   let gameData = route.params.gameData;
+  let choosePlayer = gameData.wordPlayer;
 
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{ flex: 1 }}>
         <WordPhasePrompt playerName={gameData.playerNames[0]} phrase={gameData.playerWord}>
         </WordPhasePrompt>
+      </View>
+      <View style={{ flex: 4, backgroundColor: 'red' }}>
+        <CardPicker
+          deck={gameData.playerDecks[choosePlayer]}
+          onCardSelect={(url, index) => {
+            gameData.chosenImages[choosePlayer] = gameData.playerDecks[choosePlayer][index];
+            console.log(`${gameData.playerNames[choosePlayer]} chose ${url}`);
+          }}
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+
       </View>
     </View >
   );
@@ -20,6 +34,10 @@ ChoosePhaseScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+  },
   wordMa: {
     fontSize: 20,
   }
